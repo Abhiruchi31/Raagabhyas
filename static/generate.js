273b88c3d68793297d0259e74a257d2e71866d05
Abhiruchi31 <86729585+Abhiruchi31@users.noreply.github.com>
2022-12-15 13:14:31 +0530

    Add files via upload

---
+++ static/generate.js
@@ -0,0 +1,134 @@
+function appendRestToSeed() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + "r  ";
+}
+
+function appendDurationToSeed() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + "_  ";
+}
+
+function appendToSeed48() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("सा़").id + "  ";
+}
+
+function appendToSeed49() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("C#3").id + "  ";
+}
+
+function appendToSeed50() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("D3").id + "  ";
+}
+
+function appendToSeed51() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("D#3").id + "  ";
+}
+
+function appendToSeed52() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("E3").id + "  ";
+}
+
+function appendToSeed53() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("F3").id + "  ";
+}
+
+function appendToSeed54() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("F#3").id + "  ";
+}
+
+function appendToSeed55() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("G3").id + "  ";
+}
+
+function appendToSeed56() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("G#3").id + "  ";
+}
+
+function appendToSeed57() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("A3").id + "  ";
+}
+
+function appendToSeed58() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("A#3").id + "  ";
+}
+
+function appendToSeed59() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("B3").id + "  ";
+}
+
+function appendToSeed60() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("C4").id + "  ";
+}
+
+function appendToSeed61() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("C#4").id + "  ";
+}
+
+function appendToSeed62() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("D4").id + "  ";
+}
+
+function appendToSeed63() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("D#4").id + "  ";
+}
+
+function appendToSeed64() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("E4").id + "  ";
+}
+
+function appendToSeed65() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("F4").id + "  ";
+}
+
+function appendToSeed66() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("F#4").id + "  ";
+}
+
+function appendToSeed67() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("G4").id + "  ";
+}
+
+function appendToSeed68() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("G#4").id + "  ";
+}
+
+function appendToSeed69() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("A4").id + "  ";
+}
+
+function appendToSeed70() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("A#4").id + "  ";
+}
+
+function appendToSeed71() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("B4").id + "  ";
+}
+
+function appendToSeed72() {
+    let og_seed = document.getElementById("seed").value;
+    document.getElementById("seed").value = og_seed + document.getElementById("C5").id + "  ";
+}
